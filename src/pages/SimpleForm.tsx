f2de824a1d3616003edb308698707276638fe666
Hossein <niyarakihossein@gmail.com>
2026-08-19 11:00:02 +0330

    add branch and new stat

--- src/pages/SimpleForm.tsx
+++ src/pages/SimpleForm.tsx
@@ -17,6 +17,7 @@ const ThemeForm = () => {
   const [Inpt, setInpt] = useState<string>("");
   const [Item, setItem] = useState<Item[]>([]);
   const [EditText, setEditText] = useState<string>("");
+  const [EditingId, setEditingId] = useState<number | null>(null);
 
 
   const handlechange = (e:ChangeEvent<HTMLInputElement>)=>{
